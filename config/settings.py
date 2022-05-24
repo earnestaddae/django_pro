51d8c7a51ee407088c3cef9528a2959bf9345ac6
Ernest Addae <earnestaddae@gmail.com>
2022-05-24 09:01:02 +0800

    Add static directory

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # for local development
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # for production
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.jinja'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.jinja'
