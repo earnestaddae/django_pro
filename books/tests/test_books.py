aff675c6692d0124fb244743920d575b877e73b9
Ernest Addae <earnestaddae@gmail.com>
2022-05-29 21:43:07 +0800

    Add review and associate it with book

--- books/tests/test_books.py
+++ books/tests/test_books.py
@@ -1,7 +1,8 @@
 import pytest
 from django.urls import reverse, resolve
 from pytest_django.asserts import assertTemplateUsed
-from ..models import Book
+from django.contrib.auth import get_user_model
+from ..models import Book, Review
 
 pytestmark = pytest.mark.django_db
 
@@ -15,6 +16,20 @@ def create_book():
     )
     return book
 
+@pytest.fixture
+def create_review(create_book):
+    user = get_user_model().objects.create_user(
+        username='reviewer',
+        email='reviewer@email.com',
+        password='passme123',
+    )
+    review = Review.objects.create(
+        book = create_book,
+        author = user,
+        review='I am reviewed',
+    )
+    return review
+
 class TestBook:
     def test_book_listing(self, create_book):
         assert create_book.title == 'The Denial of Death'
@@ -26,10 +41,12 @@ class TestBook:
         assert response.status_code == 200
         assertTemplateUsed(response, 'books/book_list.jinja')
 
-    def test_succesful_book_detail_view(self, client, create_book):
+    def test_succesful_book_detail_view(self, client, create_book, create_review):
         response = client.get(create_book.get_absolute_url())
         assert response.status_code == 200
         assertTemplateUsed(response, 'books/book_detail.jinja')
+        assert create_review.book == create_book
+        assert create_review.author.username == 'reviewer'
 
     def test_unsucceful_book_detail_view(self, client):
         response = client.get('/books/4321')
